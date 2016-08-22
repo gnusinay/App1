9a6c46a1d87bf2a19d26b3cababd93bfa22bbef2
Alexey Gnusin <gnusin@mercurydevelopment.com>
2016-08-22 18:20:50 +0400

    Первая рабочая версия приложения

---
+++ app/src/main/java/com/mercury/gnusin/app1/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.mercury.gnusin.app1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ArrayList<String> items = new ArrayList<>(20);
+        for (int i = 1; i <= 20; i++) {
+            items.add("Item " + i);
+        }
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/mercury/gnusin/app1/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.mercury.gnusin.app1;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView image = (ImageView) findViewById(R.id.image);
+        image.setImageResource(R.drawable.cat);
+
+       /* new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, 2000);
+*/
+    }
+}
